fb144a05890572898f6b5d760641b633a3d21c25
DJANGO LEARN <djangolearn.git@gmail.com>
2020-04-06 11:18:12 +0530

    wsgi updated

--- djangolearn/wsgi.py
+++ djangolearn/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangolearn project.
+WSGI config for Django3Project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
-
+#final
+sys.path.append('/var/projects/venv/django-learn')
+sys.path.append('/var/projects/venv/lib/python3.6/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangolearn.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
